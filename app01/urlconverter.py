4ba58645533a479c0218cbe8adfcddd79b46c2a6
mosson <136782643@qq.com>
2018-12-24 07:36:52 +0800

    app01 自定义转换器

---
+++ app01/urlconverter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'mosson'
+
+
+class FourDigitYearConverter:
+    regex = '[0-9]{4}'
+    def to_python(self, value):
+        return int(value)
+    def to_url(self, value):
+        return '%04d' % value
