b486c0a2ea84307f9e1a5d111592b567a8a50bb8
mosson <136782643@qq.com>
2018-12-24 07:37:26 +0800

    app01 views

--- app01/views.py
+++ app01/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,HttpResponse
+from django.urls import reverse # 用于url的反向解析
 
 # Create your views here.
 
@@ -11,3 +12,22 @@ def special_case_2003(request):
 def login(request):
 
     return render(request, 'login.html')
+
+def login_reverse(request):
+    """
+    URL反向解析
+    """
+
+    h = reverse('LOGIN') # 通过reverse配合url中的name就可以获得 当前的url
+
+    return render(request, 'login_reverse.html')
+
+
+def app_namespace_url(request):
+    pat = reverse('app01:app_namespace_url')
+    print(pat)
+    return HttpResponse("app01 app_namespace_url")
+
+def app01_url_converter_month(request, month):
+
+    return HttpResponse(month)
