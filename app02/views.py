1024e951653761a5673b196dbf6de2a3741cda68
mosson <136782643@qq.com>
2018-12-24 07:36:17 +0800

    app02 views

--- app02/views.py
+++ app02/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, HttpResponse
-
+from django.urls import reverse
 # Create your views here.
 def login(request):
     return HttpResponse("APP02")
+
+def app_namespace_url(request):
+    pat = reverse('app02:app_namespace_url')
+    print(pat)
+    return HttpResponse("app02 app_namespace_url")
