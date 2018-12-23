3155e462e3b84c360643c581ec853ad4bd3e89c3
mosson <136782643@qq.com>
2018-12-24 07:37:43 +0800

    app01 url

--- app01/url.py
+++ app01/url.py
@@ -2,9 +2,16 @@
 # -*- coding:utf-8 -*-
 __author__ = 'mosson'
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path, re_path, include, register_converter
 
 from app01 import views
+
+# url转换器注册
+# 导入转换器类
+from app01.urlconverter import FourDigitYearConverter
+# 注册转换器
+register_converter(FourDigitYearConverter, "myconverter")
+
 
 # 这个是全局的 应该做url的分发
 urlpatterns = [
@@ -13,5 +20,11 @@ urlpatterns = [
     # re_path(r'^articles/([0-9]{4})/$', views.year_archive),
     # re_path(r'^articles/([0-9]{4})/([0-9]{2})/$', views.month_archive),
     # re_path(r'^articles/([0-9]{4})/([0-9]{2})/([0-9]+)/$', views.article_detail),
-    path(r'login/', views.login)
+    path(r'login/', views.login),
+    path(r'app01_login_reverse/', views.login_reverse, name="LOGIN"),
+    # 名称空间app01
+    # 在不同的app的url里面定义了相同的name，可能会导致url反解错误，使用名称空间进行解决
+    path(r'app_namespace_url/', views.app_namespace_url, name='app_namespace_url'),
+    # url的转换器
+    path(r'app01_url_converter_month/<int:month>/', views.app01_url_converter_month)
 ]
