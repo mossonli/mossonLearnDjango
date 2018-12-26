42206f5962d7faa61003b1c0f3897ee7bc87ddd6
mosson <136782643@qq.com>
2018-12-26 19:55:36 +0800

    mysql数据库配置

--- mossonLearnDjango/settings.py
+++ mossonLearnDjango/settings.py
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'mossonLearnDjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+# mysql数据库配置
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'book', # 要连接的数据库，连接前需要创建好
+        'USER':'root',# 连接数据库的用户名
+        'PASSWORD':'',# 连接数据库的密码
+        'HOST':'127.0.0.1',       # 连接主机，默认本级
+        'PORT':3306 #  端口 默认3306
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
